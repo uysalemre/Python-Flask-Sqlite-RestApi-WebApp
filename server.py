69ff26e5788b760773d59b33537fb89f0c70b797
EMRE UYSAL <32219894+uysalemre@users.noreply.github.com>
2018-12-28 02:16:53 +0300

    Updated now able to run tests

--- server.py
+++ server.py
@@ -2,15 +2,18 @@ from flask import  Flask
 from flask_admin import  Admin
 from pages import website
 from flask_sqlalchemy import SQLAlchemy
-from database import db_session,init_db
+from database import db_session,init_db,delete_db
 from flask_security import Security, SQLAlchemyUserDatastore
 from models import *
 from datetime import timedelta
 from flask_mail import Mail
 from pages import AdminView,MyAdminIndexView,MyFileView
 import os.path as op
+from testFiles.database import init_db as initer
+from testFiles.database import delete_db as deleter
 
-#app = Flask(__name__) # for testing put the app into glo
+
+
 
 def create_app():
     app = Flask(__name__)
@@ -54,6 +57,49 @@ def create_app():
         db_session.remove()
     return app
 
+
+
+def create_test_app():
+    deleter()
+    initer()
+    app = Flask(__name__)
+    app.config.from_object('test_settings')
+    ##------PAGE DEVIDING -----#
+    app.register_blueprint(website)
+    ##---SECRET KEY -----#
+    app.secret_key = app.config.get('secret_key', 'secret')
+    ##---MAIL SENDER --##
+    app.sender = app.config.get('MAIL_USERNAME')
+    app.mail = Mail(app)
+    ##--SQL ALCHEMY CONFIGURAION--##
+    app.db = SQLAlchemy(app)
+    delete_db()
+    init_db()
+    # flask administration
+    path = op.join(op.dirname(__file__), 'static')
+    app.admin = Admin(app, name="Casting Agency", template_mode='bootstrap3', index_view=MyAdminIndexView())
+    app.admin.add_view(AdminView(Categories, app.db.session))
+    app.admin.add_view(AdminView(User, app.db.session, category='User Ops'))
+    app.admin.add_view(AdminView(Role, app.db.session, category='User Ops'))
+    app.admin.add_view(AdminView(Agency, app.db.session, category='App Manager'))
+    app.admin.add_view(AdminView(NormalContact, app.db.session, category='App Manager'))
+    app.admin.add_view(AdminView(Setup, app.db.session, category='App Manager'))
+    app.admin.add_view(AdminView(StaticNav, app.db.session, category='App Manager'))
+    app.admin.add_view(AdminView(Actors, app.db.session, category='Actor Ops'))
+    app.admin.add_view(AdminView(ActorBody, app.db.session, category='Actor Ops'))
+    app.admin.add_view(AdminView(ActorSoul, app.db.session, category='Actor Ops'))
+    app.admin.add_view(AdminView(Photos, app.db.session, category='Actor Ops'))
+    app.admin.add_view(AdminView(TempActors, app.db.session, category='Coming Applies'))
+    app.admin.add_view(AdminView(TempActorBody, app.db.session, category='Coming Applies'))
+    app.admin.add_view(AdminView(TempActorSoul, app.db.session, category='Coming Applies'))
+    app.admin.add_view(AdminView(TempPhotos, app.db.session, category='Coming Applies'))
+    app.admin.add_view(MyFileView(path, '/static/', name='Static Files'))
+    ##--FOR SESSIONS LIFETIME VALUE --##
+    app.permanent_session_lifetime = timedelta(hours=10)
+    ##-------SETUP FOR FLASK-SECURITY--------##
+    app.user_datastore = SQLAlchemyUserDatastore(app.db, User, RolesUsers)
+    app.security = Security(app, app.user_datastore)
+    return app
 
 def main():
     app=create_app()
